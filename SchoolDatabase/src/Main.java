f9c4678f2305c80822610a923c079dc3c441c9b4
bossLloyd <semblantelloyd500@gmail.com>
2022-11-09 14:22:14 +0800

    Hello

--- SchoolDatabase/src/Main.java
+++ SchoolDatabase/src/Main.java
@@ -3,9 +3,8 @@ import java.util.ArrayList;
 public class Main {
 	public static void main(String[] args) {
 		DB.createTables();
-		
+		System.out.println("Students: ");
 		ArrayList<Student> studentlist = DB.getStudents();
-		
 		for(Student s : studentlist) {
 			System.out.println(s.toString());
 		}
